64882cb80a465e66b5f780020f1a42b6a54d26ff
Janis Krasemann <janis.krasemann@enpit.de>
2015-04-14 14:57:38 +0200

    shortcut help is now also altered when pressing '?'

--- data/shortcuts.js
+++ data/shortcuts.js
@@ -10,6 +10,21 @@ var insertHelpText = function(element, text) {
   }
 };
 
+var alterShortcutHelp = function() {
+  var keyboardKeys = document.getElementsByClassName("keyboard-key");
+  for (var i = keyboardKeys.length - 1; i >= 0; i--) {
+    if (keyboardKeys[i].innerHTML === "&lt;") {
+      keyboardKeys[i].innerHTML = ",";
+      insertHelpText(keyboardKeys[i], "HelloTrello Anwender können , beziehungsweise . benutzen.");
+    } else if (keyboardKeys[i].innerHTML === "&gt;") {
+      keyboardKeys[i].innerHTML = ".";
+    } else if (keyboardKeys[i].innerHTML === "/") {
+      keyboardKeys[i].innerHTML = "-";
+      insertHelpText(keyboardKeys[i], "HelloTrello Anwender können - benutzen.");
+    }
+  };
+};
+
 document.onkeypress = function(e) {
 
     var searchKey = 45; // -
@@ -32,17 +47,24 @@ document.onkeypress = function(e) {
     return true;
   };
 
+document.onkeyup = function(e) {
+  var searchKey = 63; // ?
+  var evt = e || window.event;
+  var character = evt.keyCode;
+  var nodeType = evt.target.nodeName.toUpperCase();
+ 
+  if (character === 0 ||
+      evt.target.contentEditable.toUpperCase() === "TRUE" ||
+      nodeType === "TEXTAREA" ||
+      nodeType === "INPUT" && evt.target.type.toUpperCase() === "TEXT") {
+    return true; //do not detect the key if it was pressed in a textbox or sth similar
+  }
+  if (character === searchKey) {
+    alterShortcutHelp();
+  }
+  return true;
+};
+
 if (document.getElementsByClassName("shortcut-list")) {
-  var keyboardKeys = document.getElementsByClassName("keyboard-key");
-  for (var i = keyboardKeys.length - 1; i >= 0; i--) {
-    if (keyboardKeys[i].innerHTML === "&lt;") {
-      keyboardKeys[i].innerHTML = ",";
-      insertHelpText(keyboardKeys[i], "HelloTrello Anwender können , beziehungsweise . benutzen.");
-    } else if (keyboardKeys[i].innerHTML === "&gt;") {
-      keyboardKeys[i].innerHTML = ".";
-    } else if (keyboardKeys[i].innerHTML === "/") {
-      keyboardKeys[i].innerHTML = "-";
-      insertHelpText(keyboardKeys[i], "HelloTrello Anwender können - benutzen.");
-    }
-  };
+  alterShortcutHelp();
 }
